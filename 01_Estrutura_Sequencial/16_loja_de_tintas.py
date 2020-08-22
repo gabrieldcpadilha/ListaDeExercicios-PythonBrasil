518fca22ab509830c1d94bd0bb59cf756e4a86b1
Gabriel Padilha <gdcpadilha@outlook.com>
2020-08-22 20:22:04 -0300

    add exercicio 16_loja_de_tintas

--- 01_Estrutura_Sequencial/16_loja_de_tintas.py
+++ 01_Estrutura_Sequencial/16_loja_de_tintas.py
@@ -4,3 +4,13 @@ Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados
 e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
 Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 """
+area = float(input('Informe o tamanho da área (metros) a ser pintada: '))
+
+cobertura = area / 3
+latas = cobertura / 18
+
+if cobertura % 18 != 0:
+    latas += 1
+
+print(f'Quantidade de latas de tinta: {latas}')
+print(f'Preço total: R${latas * 80}')
