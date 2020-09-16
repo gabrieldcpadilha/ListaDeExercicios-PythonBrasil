e5c92d4961de24efb8360e23cf93fd7a89b675c1
gabrielpadilha-saru <gdcpadilha@outlook.com>
2020-09-16 13:48:18 -0300

    Exercicios Estrutura de Decisão

---
+++ 02_Estrutura_de_Decisao/26_posto_gasolina.py
@@ -0,0 +1,40 @@
+"""
+Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+
+Álcool:
+    até 20 litros, desconto de 3% por litro
+    acima de 20 litros, desconto de 5% por litro
+Gasolina:
+    até 20 litros, desconto de 4% por litro
+    acima de 20 litros, desconto de 6% por litro
+
+Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível
+    (codificado da seguinte forma: A-álcool, G-gasolina),
+
+calcule e imprima o valor a ser pago pelo cliente sabendo-se que
+    o preço do litro da gasolina é R$ 2,50
+    o preço do litro do álcool é R$ 1,90.
+"""
+
+# Solicita informacoes
+tipoCombustivel = str(input('Informe (A) para Alcool ou (G) para Gasolina: '))
+qtdLitros = float(input('Informe a quantidade de litros: '))
+
+# Define o valor e os descontos
+if tipoCombustivel == 'Aa':
+    valor = 1.9
+    if qtdLitros <= 20:
+        desconto = 3
+    else:
+        desconto = 5
+else:
+    valor = 2.5
+    if qtdLitros <= 20:
+        desconto = 4
+    else:
+        desconto = 6
+
+# Calcula o total
+totalPagar = (valor * qtdLitros) * ((100 - desconto) / 100.0)
+
+print (f'Total a pagar: {totalPagar}')
