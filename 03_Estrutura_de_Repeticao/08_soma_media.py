420d559be7ca4ae940ffafd8f03f4fe3cdb8c085
Gabriel Padilha <gdcpadilha@outlook.com>
2021-03-23 08:20:18 -0300

    Inclusão de exercicios resolvidos

---
+++ 03_Estrutura_de_Repeticao/08_soma_media.py
@@ -0,0 +1,7 @@
+soma = 0
+for n in range(0, 5):
+    numero = int(input('Informe um número: '))
+    soma += numero
+
+print(f'A soma dos numeros é: {soma}')
+print(f'A media dos numeros é: {soma/5}')
---
+++ 03_Estrutura_de_Repeticao/09_invertido.py
@@ -0,0 +1,6 @@
+def retorno_invertido(valor):
+    return valor[::-1]
+
+
+numero = int(input('Informe um numero: '))
+print(retorno_invertido(numero))
